9c0e098c23c99942833957021eb056e95f71ff2a
BarreiroLindoF <flavio_slb4@hotmail.com>
2017-12-21 11:33:03 +0100

    Test Prettier

--- red-cloud/App.js
+++ red-cloud/App.js
@@ -4,8 +4,7 @@ import {RkButton} from 'react-native-ui-kitten';
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
+    return (<View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
