61ed59765ece57da208429cce98167648a82abcd
Lucas Gulen <lucas.gulen@hotmail.ch>
2017-12-21 16:15:50 +0100

    Structure de la vue -création d'équipe-

--- red-cloud/App.js
+++ red-cloud/App.js
@@ -1,25 +1,74 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { RkButton } from 'react-native-ui-kitten'
+import { View, ScrollView, Keyboard } from 'react-native'
+import { RkButton, RkText, RkTextInput, RkStyleSheet, RkTheme, RkAvoidKeyboard } from 'react-native-ui-kitten'
 
 export default class App extends React.Component {
 	render() {
 		return (
-			<View style={styles.container}>
-				<Text>Open up App.js to start working on your app!</Text>
-				<Text>Changes you make will automatically reload.</Text>
-				<Text>Shake your phone to open the developer menu.</Text>
-				<RkButton rkType="outline small">Say Hello</RkButton>
-			</View>
+			<RkAvoidKeyboard
+				style={styles.screen}
+				onStartShouldSetResponder={(e) => true}
+				onResponderRelease={(e) => Keyboard.dismiss()}
+			>
+				<View>
+					<RkText rkType="title">Création de ton compte</RkText>
+				</View>
+				<View style={styles.content}>
+					<ScrollView>
+						<RkTextInput rkType="success" placeholder="Nom" />
+						<RkTextInput rkType="success" placeholder="Prénom" />
+						<RkTextInput rkType="success" placeholder="Pseudo" />
+						<RkTextInput rkType="success" placeholder="Ville" />
+						<RkTextInput rkType="success" placeholder="NPA" />
+						<RkTextInput rkType="success" placeholder="Date de naissance" />
+						<RkTextInput rkType="success" placeholder="Email" />
+						<RkTextInput rkType="success" placeholder="Password" secureTextEntry={true} />
+						<RkTextInput rkType="success" placeholder="Confirm Password" secureTextEntry={true} />
+						{/*<GradientButton style={styles.save} rkType='large' text='SIGN UP'/>*/}
+					</ScrollView>
+					<View style={styles.footer}>
+						<View style={styles.textRow}>
+							<RkText rkType="primary3">Vous avez déjà un compte ?</RkText>
+							<RkButton rkType="clear">
+								<RkText> Connectez-vous ici </RkText>
+							</RkButton>
+						</View>
+					</View>
+				</View>
+			</RkAvoidKeyboard>
 		)
 	}
 }
 
-const styles = StyleSheet.create({
-	container: {
+RkTheme.setType('RkText', 'title', {
+	fontSize: 20,
+	paddingTop: '100px',
+})
+
+let styles = RkStyleSheet.create((theme) => ({
+	screen: {
+		padding: 16,
 		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
+		justifyContent: 'space-around',
+		backgroundColor: theme.colors.screen.base,
+	},
+	content: {
+		justifyContent: 'space-between',
+	},
+	save: {
+		marginVertical: 20,
+	},
+	buttons: {
+		flexDirection: 'row',
+		marginBottom: 24,
+		marginHorizontal: 24,
+		justifyContent: 'space-around',
+	},
+	footer: {
+		justifyContent: 'flex-end',
+	},
+	textRow: {
+		flexDirection: 'row',
 		justifyContent: 'center',
 	},
-})
+}))
