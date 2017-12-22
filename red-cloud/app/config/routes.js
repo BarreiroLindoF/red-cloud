5a5dc2e6f9e8f49da2a8818f1ab21a0470e74696
Lucas Gulen <lucas.gulen@hotmail.ch>
2017-12-22 17:13:07 +0100

    Navigation couplée avec -Signup-

--- red-cloud/app/config/routes.js
+++ red-cloud/app/config/routes.js
@@ -4,4 +4,5 @@ import { StackNavigator } from 'react-navigation'
 export const Routes = StackNavigator({
 	Login: { screen: Views.Login },
 	Exemple: { screen: Views.Exemple },
+	Signup: { screen: Views.Signup },
 })
