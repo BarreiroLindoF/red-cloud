43ad50412931c7d54d23332d333ce5d420b8476d
Lucas Gulen <lucas.gulen@hotmail.ch>
2018-01-07 21:03:19 +0100

    Deux versions signup et début validation champs

--- red-cloud/app/views/login/signup.js
+++ red-cloud/app/views/login/signup.js
@@ -1,37 +1,104 @@
 import React from 'react';
 import { View, ScrollView, KeyboardAvoidingView } from 'react-native';
-import { RkButton, RkText, RkTextInput, RkStyleSheet, RkTheme } from 'react-native-ui-kitten';
+import { RkButton, RkText, RkStyleSheet } from 'react-native-ui-kitten';
+import { Hoshi } from 'react-native-textinput-effects';
+
+const regPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+const regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 export class Signup extends React.Component {
 	//eslint-disable-next-line
 	static navigationOptions = {
 		title: 'Création de ton compte',
 	};
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			pass: '',
+			validationPass: '',
+			passOk: false,
+			validationPassOk: true,
+			validationEmail: true,
+		};
+	}
+
+	validate(input) {
+		this.setState({ pass: input });
+		this.compareMdp(this.state.validationPass);
+		if (regPassword.test(input)) {
+			this.setState({ passOk: true });
+		} else {
+			this.setState({ passOk: false });
+		}
+	}
+
+	compareMdp(input) {
+		this.setState({ validationPass: input });
+		if (input == this.state.pass) {
+			this.setState({ validationPassOk: true });
+		} else {
+			this.setState({ validationPassOk: false });
+		}
+	}
+
+	validateEmail(input) {
+		if (regEmail.test(input)) {
+			this.setState({ validateEmail: true });
+		} else {
+			this.setState({ validateEmail: false });
+		}
+	}
+
 	render() {
 		const { navigate } = this.props.navigation;
 		return (
 			<KeyboardAvoidingView style={styles.screen} behavior="padding" keyboardVerticalOffset={55}>
-				<View style={{ flex: 2 }}>
+				<View
+					style={{
+						flex: 1,
+						paddingLeft: 10,
+						flexDirection: 'column',
+						justifyContent: 'space-between',
+					}}
+				>
 					<ScrollView keyboardShouldPersistTaps="handled" keyboardDismissMode="interactive">
-						<RkTextInput rkType="success" placeholder="Nom" />
-						<RkTextInput rkType="success" placeholder="Prénom" />
-						<RkTextInput rkType="success" placeholder="Pseudo" />
-						<RkTextInput rkType="success" placeholder="Ville" />
-						<RkTextInput rkType="success" placeholder="NPA" />
-						<RkTextInput rkType="success" placeholder="Date de naissance" />
-						<RkTextInput rkType="success" placeholder="Email" />
-						<RkTextInput rkType="success" placeholder="Password" secureTextEntry />
-						<RkTextInput
-							style={styles.font}
-							rkType="success"
-							placeholder="Confirm Password"
+						<Hoshi label={'Nom'} borderColor={'grey'} />
+						<Hoshi label={'Prénom'} borderColor={'grey'} />
+						<Hoshi label={'Pseudo'} borderColor={'grey'} />
+						<Hoshi
+							label={'Email'}
+							borderColor={'grey'}
+							keyboardType="email-address"
+							borderColor={this.state.validationEmail ? 'grey' : '#ff4444'}
+							onChangeText={(input) => {
+								this.validateEmail(input);
+							}}
+						/>
+						<Hoshi label={'NPA'} borderColor={'grey'} keyboardType="numeric" />
+						<Hoshi label={'Ville'} borderColor={'grey'} />
+						<Hoshi label={'Date de naissance'} borderColor={'grey'} keyboardType="numeric" />
+						<Hoshi
+							label={'Mot de passe'}
+							borderColor={this.state.passOk ? 'grey' : '#ff4444'}
+							onChangeText={(input) => {
+								this.validate(input);
+							}}
+							secureTextEntry
+						/>
+						<Hoshi
+							label={'Validation mot de passe'}
+							borderColor={this.state.validationPassOk ? 'grey' : '#ff4444'}
+							onChangeText={(input) => {
+								this.compareMdp(input);
+							}}
 							secureTextEntry
 						/>
 					</ScrollView>
 				</View>
 				<View style={styles.save}>
-					<RkButton style={{ backgroundColor: 'green' }} rkType="rounded">
-						Suivant
+					<RkButton style={{ backgroundColor: 'white' }} rkType="social">
+						<RkText style={{ color: 'black' }}>Suivant</RkText>
 					</RkButton>
 				</View>
 				<View style={styles.footer}>
@@ -45,7 +112,7 @@ export class Signup extends React.Component {
 								navigate('Login');
 							}}
 						>
-							<RkText style={{ color: 'white' }}> Connectez-vous ici </RkText>
+							<RkText style={{ color: 'red' }}> Connectez-vous ici </RkText>
 						</RkButton>
 					</View>
 				</View>
@@ -54,23 +121,12 @@ export class Signup extends React.Component {
 	}
 }
 
-RkTheme.setType('RkTextInput', 'success', {
-	labelColor: 'grey',
-	underlineColor: 'grey',
-	underlineWidth: 2,
-	placeholderTextColor: 'grey',
-	color: 'white',
-});
-
 //eslint-disable-next-line
 let styles = RkStyleSheet.create(() => ({
 	screen: {
 		padding: 10,
 		flex: 1,
-		justifyContent: 'space-around',
 		backgroundColor: 'black',
-	},
-	content: {
 		justifyContent: 'space-between',
 	},
 	save: {
