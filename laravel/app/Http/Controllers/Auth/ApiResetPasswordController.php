a375ffa72ea59514aafc9f09caa796980f810d67
BarreiroLindoF <flavio_slb4@hotmail.com>
2018-02-22 10:29:09 +0100

    Bugfix : status < 1 not allowed in token check

--- laravel/app/Http/Controllers/Auth/ApiResetPasswordController.php
+++ laravel/app/Http/Controllers/Auth/ApiResetPasswordController.php
@@ -22,6 +22,9 @@ class ApiResetPasswordController extends Controller
         if ($passwordRecovery->delaiDepasse()) {
             return response()->json(new JsonResponse(false, null, 'Délai de récupération de mot de passe dépassé !'));
         }
+        if ($passwordRecovery->getAttribute('status') < 1) {
+            return response()->json(new JsonResponse(false, null, 'Le code n\' pas encore été utilisé !'));
+        }
         if ($passwordRecovery->getAttribute('status') > 1) {
             return response()->json(new JsonResponse(false, null, 'Ce token a déjà été utilisé !'));
         }
@@ -30,7 +33,7 @@ class ApiResetPasswordController extends Controller
         }
         $passwordRecovery->setAttribute('status', 2);
         $passwordRecovery->save();
-        // Pas besoin de vérifier le mail sinon il n'y aura pas de demande de mot de passe
+        // Pas besoin de vérifier le mail sinon il n'y aurait pas de demande de mot de passe
         // et donc, on se serait arrêté plus haut
         User::where('email', $email)->update(['password' => bcrypt($password)]);
         return response()->json(new JsonResponse(true, null, "Mot de passe modifié avec succès !"));
