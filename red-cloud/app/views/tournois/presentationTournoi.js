ffde1e9d5a17d22cf7eaf5d599b122a20c52448f
Lucas Gulen <lucas.gulen@hotmail.ch>
2018-03-06 20:10:02 +0100

    refactoring de la vue d'un tournoi et style extraits au bas de la vue

--- red-cloud/app/views/tournois/presentationTournoi.js
+++ red-cloud/app/views/tournois/presentationTournoi.js
@@ -3,6 +3,8 @@ import { View, Text, ScrollView, TouchableOpacity, Image, Linking, Share } from 
 import { RkButton, RkText } from 'react-native-ui-kitten';
 
 const Dimensions = require('Dimensions');
+
+const proportionImageScrollView = 1.7;
 const FaceBookImgSrc = require('../../assets/images/facebook-logo.png');
 const YoutubeImgSrc = require('../../assets/images/youtube-logo.png');
 const TwitchImgSrc = require('../../assets/images/twitch-logo.png');
@@ -21,7 +23,7 @@ const StoreAppUrl = 'https://play.google.com/store/apps/details?id=com.bigredclo
 class PresentationTournoi extends React.Component {
 	//eslint-disable-next-line
 	static navigationOptions = {
-		title: '',
+		title: 'Tournoi au RedCloud',
 	};
 
 	constructor(props) {
@@ -29,120 +31,61 @@ class PresentationTournoi extends React.Component {
 		this.state = {
 			reglement:
 				'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \n\n Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \n\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. \n\nNemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. \n\n',
+			heightScrollViewDisplayed: 420,
 		};
 	}
 
 	render() {
+		const tournoi = this.props.navigation.state.params.tournoi;
 		return (
-			<View style={{ flex: 1, backgroundColor: 'white' }}>
-				<View
-					style={{
-						backgroundColor: '#cc0000',
-						paddingBottom: 10,
-						paddingTop: 10,
-						flexDirection: 'column',
-						justifyContent: 'center',
-						alignItems: 'flex-start',
-						borderColor: 'black',
-						borderBottomWidth: 1,
-						borderTopWidth: 1,
-					}}
-				>
-					<Text
-						style={{
-							color: 'white',
-							backgroundColor: 'black',
-							padding: 10,
-							fontWeight: 'bold',
-							fontFamily: 'monospace',
+			<View style={Styles.container}>
+				<View style={Styles.rubanHaut}>
+					<Text style={Styles.title}>{tournoi.titre}</Text>
+				</View>
+				<View style={Styles.containerScrollView}>
+					<ScrollView
+						contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}
+						onLayout={(event) => {
+							this.setState({ heightScrollViewDisplayed: event.nativeEvent.layout.height });
 						}}
 					>
-						{this.props.navigation.state.params.tournoi.titre}
-					</Text>
-				</View>
-				<View
-					style={{
-						backgroundColor: 'white',
-						flex: 1,
-						marginBottom: 10,
-					}}
-				>
-					<ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}>
 						<View>
 							<Image
-								source={{ uri: this.props.navigation.state.params.tournoi.imageUri }}
+								source={{ uri: tournoi.imageUri }}
 								style={{
 									width: Dimensions.get('window').width,
-									height: 160,
+									height: this.state.heightScrollViewDisplayed / proportionImageScrollView,
+									resizeMode: 'stretch',
 								}}
 							/>
 						</View>
 						<View>
-							<Text
-								multiline
-								style={{
-									color: 'black',
-									textAlign: 'justify',
-									lineHeight: 20,
-									paddingTop: 10,
-								}}
-							>
+							<Text multiline style={Styles.text}>
 								{this.state.reglement}
 							</Text>
 						</View>
-						<View
-							style={{
-								flexDirection: 'row',
-								justifyContent: 'center',
-							}}
-						>
-							<RkButton
-								rkType="social"
-								style={{
-									backgroundColor: 'black',
-								}}
-							>
-								<RkText
-									rkType="awesome hero accentColor"
-									style={{
-										color: 'white',
-										fontWeight: 'bold',
-									}}
-								>
+						<View style={Styles.btnContainer}>
+							<RkButton rkType="social" style={Styles.btn}>
+								<RkText rkType="awesome hero accentColor" style={Styles.fontBtn}>
 									Je m'inscris !
 								</RkText>
 							</RkButton>
 						</View>
 					</ScrollView>
 				</View>
-				<View
-					style={{
-						flexDirection: 'row',
-						justifyContent: 'center',
-					}}
-				>
-					<Text
-						style={{
-							width: 250,
-							paddingBottom: 10,
-							marginTop: 10,
-							borderTopColor: '#cc0000',
-							borderTopWidth: 2,
-						}}
-					/>
+				<View style={Styles.bottomLineContainer}>
+					<Text style={Styles.bottomLine} />
 				</View>
-				<View
-					style={{
-						flexDirection: 'row',
-						justifyContent: 'space-around',
-						marginBottom: 25,
-						paddingLeft: 35,
-						paddingRight: 35,
-					}}
-				>
+				<View style={Styles.socialFooter}>
 					<TouchableOpacity
 						onPress={() => {
-							Linking.openURL(FaceBookPage);
+							/*Linking.openURL(FaceBookPage);*/
+							Share.share({
+								message: `RedCloud est désromais dans ton Store ! N'attends plus et viens rejoindre tes amis et rivaux :)!
+                                    ${Android ? ' ' : ' '}`,
+								title: 'App RedCloud',
+								image: tournoi.imageUri,
+							});
 						}}
 					>
 						<Image source={FaceBookImgSrc} style={Styles.logo} />
@@ -151,11 +94,11 @@ class PresentationTournoi extends React.Component {
 					<TouchableOpacity
 						onPress={() => {
 							Share.share({
-								message: Android
-									? `RedCloud est désromais dans ton Store ! N'attends plus et viens rejoindre tes amis et rivaux :)!" ${StoreAppUrl}`
-									: "RedCloud est désromais dans ton Store ! N'attends plus et viens rejoindre tes amis et rivaux :)!",
+								message: `RedCloud est désromais dans ton Store ! N'attends plus et viens rejoindre tes amis et rivaux :)!
+                                    ${Android ? tournoi.imageUri : ''}`,
 								url: StoreAppUrl,
 								title: 'App RedCloud',
+								image: tournoi.imageUri,
 							});
 						}}
 					>
@@ -201,6 +144,68 @@ let Styles = {
 		backgroundColor: 'red',
 		justifyContent: 'center',
 		width: 200,
+	},
+	container: {
+		flex: 1,
+		backgroundColor: 'white',
+	},
+	rubanHaut: {
+		backgroundColor: '#cc0000',
+		paddingBottom: 10,
+		paddingTop: 10,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'flex-start',
+		borderColor: 'black',
+		borderBottomWidth: 1,
+		borderTopWidth: 1,
+	},
+	title: {
+		color: 'white',
+		backgroundColor: 'black',
+		padding: 10,
+		fontWeight: 'bold',
+		fontFamily: 'monospace',
+	},
+	containerScrollView: {
+		backgroundColor: 'white',
+		flex: 1,
+		marginBottom: 10,
+	},
+	text: {
+		color: 'black',
+		textAlign: 'justify',
+		lineHeight: 20,
+		paddingTop: 10,
+	},
+	btnContainer: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+	},
+	btn: {
+		backgroundColor: 'black',
+	},
+	fontBtn: {
+		color: 'white',
+		fontWeight: 'bold',
+	},
+	bottomLineContainer: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+	},
+	bottomLine: {
+		width: 250,
+		paddingBottom: 10,
+		marginTop: 10,
+		borderTopColor: '#cc0000',
+		borderTopWidth: 2,
+	},
+	socialFooter: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		marginBottom: 25,
+		paddingLeft: 35,
+		paddingRight: 35,
 	},
 };
 
