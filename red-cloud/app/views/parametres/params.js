402ca2a92113b57b8e4c60362db8ce8c1f0d3e39
BarreiroLindoF <flavio_slb4@hotmail.com>
2018-04-16 11:02:08 +0200

    Modification champ deconnexion

--- red-cloud/app/views/parametres/params.js
+++ red-cloud/app/views/parametres/params.js
@@ -39,11 +39,11 @@ class Params extends React.Component {
 					this.openLoginView();
 				}}
 			>
-				<RkCard rkType="blog" style={styles.card}>
+				<RkCard rkType="blog" style={Styles.card}>
 					<View rkCardContent>
 						<RkText rkType="primary3 mediumLine">
 							<Icon size={24} color="#cc0000" name="power-settings-new" />
-							<Text>Déconnexion</Text>
+							Déconnexion
 						</RkText>
 					</View>
 				</RkCard>
@@ -53,23 +53,64 @@ class Params extends React.Component {
 
 	render() {
 		return (
-			<View>
+			<View style={Styles.container}>
 				<StatusBarPaddingView />
-				<View style={styles.container}>{this.renderDeconnexion()}</View>
+				<View style={Styles.rubanHaut}>
+					<Text style={Styles.title}>Paramètres</Text>
+				</View>
+				<StatusBarPaddingView />
+				<View style={Styles.containerCard}>{this.renderDeconnexion()}</View>
 			</View>
 		);
 	}
 }
 
-//eslint-disable-next-line
-const styles = RkStyleSheet.create((theme) => ({
-	container: {
+let Styles = RkStyleSheet.create((theme) => ({
+	containerCard: {
 		backgroundColor: theme.colors.screen.scroll,
 		paddingVertical: 8,
 		paddingHorizontal: 12,
 	},
 	card: {
 		marginVertical: 8,
+	},
+	centerContent: {
+		alignItems: 'center',
+	},
+	boldText: {
+		fontWeight: 'bold',
+	},
+	logo: {
+		width: 200,
+		height: 200,
+		resizeMode: 'stretch',
+	},
+	container: {
+		flex: 1,
+		backgroundColor: 'white',
+	},
+	rubanHaut: {
+		backgroundColor: '#cc0000',
+		paddingBottom: 10,
+		paddingTop: 10,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'flex-start',
+		borderColor: 'black',
+		borderBottomWidth: 1,
+		borderTopWidth: 1,
+	},
+	title: {
+		color: 'white',
+		backgroundColor: 'black',
+		padding: 10,
+		fontWeight: 'bold',
+		fontFamily: 'monospace',
+	},
+	containerScrollView: {
+		backgroundColor: 'white',
+		flex: 1,
+		marginBottom: 10,
 	},
 }));
 
