8d1d1d3f4e2ba7815c707d0ce2a515a208a441d8
SteeveGerson <steeve.gerson@etu.hesge.ch>
2018-05-07 09:24:47 +0200

    Affichage menu

--- laravel/app/Http/Controllers/Menu/ApiMenuController.php
+++ laravel/app/Http/Controllers/Menu/ApiMenuController.php
@@ -6,8 +6,10 @@ use App\Nourriture;
 use App\Boisson;
 use App\Menu;
 use App\CategorieNourriture;
+use App\CategorieBoisson;
 use App\Http\Controllers\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 
 class ApiMenuController extends Controller{
@@ -15,15 +17,32 @@ class ApiMenuController extends Controller{
     public function getMenu(Request $request){
         $nourritures = Nourriture::all();
         $boissons = Boisson::all();
+        $catBoisson = CategorieBoisson::all();
+        $catNourriture = CategorieNourriture::all();
+        /*$categorie = CategorieNourriture::join('categorie_nourriture', 'categorie_nourriture_id_categorie_nourriture', 'id');
 		$menu = new Menu();
         $menu->boissons = $boissons;
         $menu->nourritures = $nourritures;
-        return response()->json(new JsonResponse(true, $menu, 'Liste de la nourritures chargées'));
-    }
+        $menu->categorieBoisson = $catBoisson;
+        $menu->categorieNourriture = $catNourriture;*/
 
-    public function getCategorie(Request $request){
-        $idCat = $request->input('idCat');
-        $categorie = CategorieNourriture::find($idCat);
-        return response()->json(new JsonResponse(true, $categorie, null));
+        /*$menu = DB::table('boisson')
+                ->join('categorie_boisson', 'id_categorie_boisson', '=', 'categorie_boisson_id_categorie_boisson')
+                ->select('boisson.nom AS boisson_nom', 'categorie_boisson.nom AS categorie_nom')
+                ->groupBy('categorie_nom', 'boisson_nom')
+                ->get();*/
+        $menu = null;
+        $categories = DB::table('categorie_boisson')
+                ->select('nom', 'id_categorie_boisson')
+                ->orderby('id_categorie_boisson')
+                ->get();
+
+        for($index=0; $index<count($categories); $index++){
+            $menu = DB::table('boisson')
+                    ->select('nom', 'prix')
+                    ->where('categorie_boisson_id_categorie_boisson', '=', $index+1)
+                    ->get();
+        }
+        return response()->json(new JsonResponse(true, $menu, 'Liste de la nourritures chargées'));
     }
 }
--- laravel/app/Menu.php
+++ laravel/app/Menu.php
@@ -4,6 +4,8 @@ namespace App;
 
 class Menu
 {
-    public $boissons;
-    public $nourritures;
+    public $categorieBoisson;
+    public $boissons = [];
+    public $categorieNourriture;
+    public $nourritures = [];
 }
